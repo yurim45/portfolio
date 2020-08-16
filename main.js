8be7ece7d3832c0b50f50c3666c954a239d270ba
yurim86 <icho0405@naver.com>
2020-08-16 11:38:58 +0900

    JS Project filtering💡

--- main.js
+++ main.js
@@ -57,6 +57,29 @@ arrowUp.addEventListener("click", () => {
     scrollIntoView("#home");
 });
 
+// projects
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+    const filter =
+        e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    projectContainer.classList.add("anim-out");
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if (filter === "*" || filter === project.dataset.type) {
+                project.classList.remove("invisible");
+            } else {
+                project.classList.add("invisible");
+            }
+        });
+        projectContainer.classList.remove("anim-out");
+    }, 300);
+});
+
 // 스크롤 이동 함수
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
