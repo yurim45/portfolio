8e9a08aaa972bd5dd9ba4834b82a4ee5920b5cf1
yurim86 <icho0405@naver.com>
2020-08-16 09:43:30 +0900

    JS Scroll to section💡

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ document.addEventListener("scroll", () => {
         navbar.classList.remove("navbar--dark");
     }
 });
+
+// handle scrolling when tapping on the navbar menu
+// navbar 메뉴를 탭할 때 스크롤 처리
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    } else {
+        const scrollTo = document.querySelector(link);
+        scrollTo.scrollIntoView({ behavior: "smooth" });
+    }
+});
