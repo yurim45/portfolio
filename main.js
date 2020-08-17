a31ee5dd137c81580442ff978cd72cac02f40966
yurim86 <icho0405@naver.com>
2020-08-17 12:33:20 +0900

    JS Navbar toggle button💡

--- main.js
+++ main.js
@@ -20,9 +20,15 @@ navbarMenu.addEventListener("click", (event) => {
     const link = target.dataset.link;
     if (link == null) {
         return;
-    } else {
-        scrollIntoView(link);
     }
+    navbarMenu.classList.remove("open");
+    scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("open");
 });
 
 // Handle contact me button 💡
